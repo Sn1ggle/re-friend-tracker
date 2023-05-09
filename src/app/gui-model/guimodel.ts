0e28c50013ba7490f3a21a8038dba25c2d782418
Sn1ggle MaBoogie <sirsniggle@gmail.com>
2023-05-09 17:54:08 +0000

    buttonGroup

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -149,6 +149,13 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+                        {   
+                            "type": "button",
+                            "name": "Groups",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "page": "groupspage",
+                        },
                     ]
                 },
                 {
